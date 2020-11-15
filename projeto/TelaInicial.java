9feeb77867ae4de9c680cb0282c8e4bcf04de019
CHubert <carloshubert3@gmail.com>
2020-11-15 13:15:41 -0300

    Alteração da Tela Inicial
    Alterada a forma de montagem da tela, utilizando uma imagem para o background completo.

--- projeto/TelaInicial.java
+++ projeto/TelaInicial.java
@@ -13,6 +13,8 @@ import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JLabel;
+import java.awt.Dimension;
+import java.awt.Frame;
 
 public class TelaInicial extends JFrame {
 
@@ -38,72 +40,54 @@ public class TelaInicial extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaInicial() {
+		setExtendedState(Frame.MAXIMIZED_BOTH);
+		setMaximumSize(new Dimension(1920, 1080));
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1236, 661);
-		contentPane = new JPanel();
+		setBounds(100, 100, 1920, 1080); //1236, 661);
+		contentPane = new JPanel(){
+			public void paintComponent(Graphics g) {
+				Image img = Toolkit.getDefaultToolkit().getImage(
+						TelaInicial.class.getResource("/imagens/background.png"));
+				g.drawImage(img, 0, 0, this.getWidth(),this.getHeight(),this);
+			}
+		};
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel pnlSlogan = new JPanel();
-		pnlSlogan.setBackground(Color.decode("#334355"));
-		pnlSlogan.setBounds(0, 0, 1227, 170);
-		contentPane.add(pnlSlogan);
-		pnlSlogan.setLayout(null);
-		
-		JPanel pnlLogo = new JPanel(){
-			public void paintComponent(Graphics g) {
-				Image img = Toolkit.getDefaultToolkit().getImage(
-						TelaInicial.class.getResource("/imagens/MOBRAL_LOGO.png"));
-				g.drawImage(img, 0, 0, this.getWidth(),this.getHeight(),this);
-			}
-		};
-		pnlLogo.setBounds(33, 0, 237, 170);
-		pnlSlogan.add(pnlLogo);
-		
-		JLabel lblSlogan = new JLabel("Slogan aqui, qualquer frase que fa\u00E7a a empresa parecer muito legal!");
-		lblSlogan.setForeground(Color.WHITE);
-		lblSlogan.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblSlogan.setBounds(356, 123, 599, 36);
-		pnlSlogan.add(lblSlogan);
-		
-		JLabel lblUsuario = new JLabel("usuario1234 | sair");
-		lblUsuario.setForeground(Color.WHITE);
-		lblUsuario.setBounds(1114, 11, 91, 33);
-		pnlSlogan.add(lblUsuario);
-		
 		JButton btnAgenda = new JButton("Agenda");
 		btnAgenda.setEnabled(false);
 		btnAgenda.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		btnAgenda.setBounds(147, 235, 250, 100);
+		btnAgenda.setBounds(308, 435, 333, 155);
 		contentPane.add(btnAgenda);
 		
 		JButton btnSimulacao = new JButton("Simula\u00E7\u00E3o de Pre\u00E7o");
 		btnSimulacao.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		btnSimulacao.setBounds(147, 416, 250, 100);
+		btnSimulacao.setBounds(308, 754, 333, 155);
 		contentPane.add(btnSimulacao);
 		
 		JButton btnHistorico = new JButton("Hist\u00F3rico de Simula\u00E7\u00F5es");
 		btnHistorico.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		btnHistorico.setBounds(512, 416, 250, 100);
+		btnHistorico.setBounds(784, 754, 333, 155);
 		contentPane.add(btnHistorico);
 		
 		JButton btnClientes = new JButton("Clientes");
 		btnClientes.setEnabled(false);
 		btnClientes.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		btnClientes.setBounds(512, 235, 250, 100);
+		btnClientes.setBounds(784, 435, 333, 155);
 		contentPane.add(btnClientes);
 		
 		JButton btnConfiguracoes = new JButton("Configura\u00E7\u00F5es");
 		btnConfiguracoes.setEnabled(false);
 		btnConfiguracoes.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		btnConfiguracoes.setBounds(862, 416, 250, 100);
+		btnConfiguracoes.setBounds(1261, 754, 333, 155);
 		contentPane.add(btnConfiguracoes);
 		
 		JButton btnRelatrios = new JButton("Relat\u00F3rios");
 		btnRelatrios.setEnabled(false);
 		btnRelatrios.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		btnRelatrios.setBounds(862, 235, 250, 100);
+		btnRelatrios.setBounds(1261, 435, 333, 155);
 		contentPane.add(btnRelatrios);
 	}
 }
